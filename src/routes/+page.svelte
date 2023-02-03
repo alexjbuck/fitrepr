19cbc8e65423d8148e84c6084e5d4e1e782ef32a
Alex Buck <alexjbuck@gmail.com>
2023-02-03 11:55:39 -0800

    adding supabase authentication

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Auth from './Auth.svelte';
+	import Account from './Account.svelte';
+</script>
+
+<svelte:head>
+	<title>Fitrepr</title>
+	<meta name="description" content="SvelteKit using supabase-js v2" />
+</svelte:head>
+
+{#if !$page.data.session}
+	<Auth />
+{:else}
+	<Account session={$page.data.session} />
+{/if}
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
