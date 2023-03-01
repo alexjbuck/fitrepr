b57dd43b63cbde54713bd931efaf9a48c5b41037
Alex Buck <alexjbuck@gmail.com>
2023-03-01 16:51:28 -0500

    Adding feedback/contact link

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,7 @@
 <footer class="p-1 footer flex flex-row justify-between text-xs">
     <span class="text-inherit">&#xa9; Alexander &quot;Jarvis&quot; Buck</span>
     <span>
+        <a href="mailto:fitrepr@alexjbuck.com">Feedback & Contact</a>
         <a href="/privacy-policy">Privacy Policy</a>
     </span>
 </footer>
